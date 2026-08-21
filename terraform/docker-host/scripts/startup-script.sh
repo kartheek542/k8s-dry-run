62888a5e7178ecb0cb3c63b14ba6d998a01c11d9
kartheek542 <kartheekdivvela@gmail.com>
2026-08-21 20:46:52 +0530

    Updated startup script

--- terraform/docker-host/scripts/startup-script.sh
+++ terraform/docker-host/scripts/startup-script.sh
@@ -8,7 +8,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
               
 apt update -y
-apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
               
 # Enable docker service and grant permissions to default ubuntu user
 jq -n '{hosts: ["unix:///var/run/docker.sock", "tcp://0.0.0.0:2375"]}' > /etc/docker/daemon.json
