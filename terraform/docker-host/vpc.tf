b2ebca936d49b88bf4eda612a8cb986e99f1307d
kartheek542 <kartheekdivvela@gmail.com>
2026-08-21 16:57:41 +0530

    Updated CI workflow with creation of docker host

--- terraform/docker-host/vpc.tf
+++ terraform/docker-host/vpc.tf
@@ -7,6 +7,7 @@ module "vpc" {
 
   region = var.aws_region
 
+  azs = ["ap-south-1a", "ap-south-1b"]
   public_subnets       = ["10.0.1.0/24"]
   enable_nat_gateway   = false
   enable_dns_hostnames = true
